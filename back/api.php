b72857a384597bd95cf16142e32e15e59d99f5a7
andronedev <andronedev@gmail.com>
2023-11-02 16:42:17 +0100

    🔒️ protection de l'api

--- back/api.php
+++ back/api.php
@@ -2,13 +2,22 @@
 
 require 'db.php';
 
-header('Access-Control-Allow-Origin: *'); 
 header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: http://127.0.0.1:3000');
+header('Access-Control-Allow-Credentials: true');
+session_start();
+
+
+$est_connecte = isset($_SESSION['user']);
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 
   if ($_GET['resource'] === 'activities') {
-    
+    if (!$est_connecte) {
+      http_response_code(401);
+      echo json_encode(['error' => 'Non autorisé']);
+      exit;
+    }
     $data = query("SELECT * FROM activites INNER JOIN planifications ON planifications.id_activite = activites.id INNER JOIN lieux ON lieux.id = planifications.id_lieu");
     //  retourner les données en JSON
     echo json_encode($data->fetchAll(PDO::FETCH_ASSOC));
--- front/pages/activites.vue
+++ front/pages/activites.vue
@@ -173,7 +173,9 @@ export default {
    * @returns {void}
    */
   mounted() {
-    fetch("http://127.0.0.1/edsa-mycoach/api.php?resource=activities")
+    fetch("http://127.0.0.1/edsa-mycoach/api.php?resource=activities",{
+      credentials: "include",
+    })
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
